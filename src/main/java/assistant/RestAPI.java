dd861f79b7c4b1ae42b835de1dbbef6edbd9b1ed
Jad L <92789238+NoAmpersand@users.noreply.github.com>
2023-03-06 12:24:10 +0100

    Hey hey hey

---
+++ src/main/java/assistant/RestAPI.java
@@ -0,0 +1,11 @@
+package assistant;
+
+
+public class RestAPI {
+    final static String clientSecretFile = "client.json";
+    final static String APIname = "calendar";
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
