b8e7b1df9da6fb0e38d2211ee7d550503242bacb
Jad L <92789238+NoAmpersand@users.noreply.github.com>
2023-03-07 17:06:08 +0100

    Initial test commit

--- src/main/java/assistant/Main.java
+++ src/main/java/assistant/Main.java
@@ -67,7 +67,6 @@ public class Main {
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
@@ -105,5 +104,6 @@ public class Main {
                 System.out.printf("%s (%s)\n", event.getSummary(), start);
             }
         }
+
     }
 }
